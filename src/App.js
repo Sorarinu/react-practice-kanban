a9fdce844ab776f9e0dcd96576f747224c17bdc0
高畑 達也 <takahata.tatsuya@willgate.co.jp>
2020-09-18 15:32:15 +0900

    feat: カードの移動ができるように

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ReactSortable } from 'react-sortablejs'
 import styled from "styled-components"
 import CardList from "./CardList"
 import Card from "./Card"
@@ -8,8 +9,8 @@ import CreateModal from './CreateModal'
 const SCardLists = styled.div`
     display: flex;
     width: 100%;
-    box-sizing: border-box;
     padding-left: 30px;
+    box-sizing: border-box;
 `
 
 const SButton = styled.button`
@@ -29,50 +30,108 @@ const SButton = styled.button`
     }
 `
 
+const SAddListButton = styled(SButton)` /* SButtonのstyleを上書きして新しいコンポーネントを作成 */
+    margin: 30px 30px 30px 0px;
+    min-width: 128px;
+`
+
+const SListTitle = styled.div`
+    font-weight: bold; /* 太字 */
+    padding: 5px; /* 適度な余白 */
+    flex-grow: 1; /* 要素を最大限伸ばす */
+    /* タイトルが長い時「...」にする */
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    min-width:0;
+`
+
+const SListTitleWrapper = styled.div`
+    display: flex; /* タイトルとボタンを横並び */
+`
+const SRemoveCardListButton = styled(SButton)`
+    font-size: 0.8rem; /* 削除ボタンの文字を少し小さく*/
+`
 
 function App() {
     const [state, setState] = useState({
-        cards: [
-            { title: "ここにタイトル" },
+        cardLists: [
+            {
+                title: 'リストタイトルA',
+                cards: [{ title: 'カードタイトル' }]
+            },
+            {
+                title: 'リストタイトルB',
+                cards: [{ title: 'カードタイトル' }]
+            },
+            {
+                title: 'リストタイトルC',
+                cards: [{ title: 'カードタイトル' }]
+            }
         ],
-        shownCreateCardModal: false
+        targetListIndex: -1, // Listを指定した動作を実現するため
+        shownCreateCardModal: false,
+        shownCreateCardListModal: false // カードリスト追加のモーダル表示制御変数
     })
 
-    const { cards, shownCreateCardModal } = state
+    const { cardLists, targetListIndex, shownCreateCardModal, shownCreateCardListModal } = state
 
-    const openCreateCardModal = () => {
-        setState({
-            ...state,
-            shownCreateCardModal: true
-        })
+    const addCardList = (newCardList) => {
+        cardLists.push({ cards: [], ...newCardList })
+        setState({ ...state, cardLists, shownCreateCardListModal: false })
+    }
+
+    const removeCardList = ( listIndex ) => {
+        if (window.confirm('削除しますか？')) {
+          cardLists.splice(listIndex, 1)
+          setState({ ...state, cardLists })
+        }
+      }
+
+    const openCreateCardListModal = () => {
+        setState({ ...state, shownCreateCardListModal: true })
+    }
+
+    const closeCreateCardListModal = () => {
+        setState({ ...state, shownCreateCardListModal: false })
+    }
+
+    const openCreateCardModal = (targetListIndex) => {
+        setState({ ...state, shownCreateCardModal: true, targetListIndex })
     }
     const closeCreateCardModal = () => {
-        setState({
-            ...state,
-            shownCreateCardModal: false
-        })
+        setState({ ...state, shownCreateCardModal: false })
+    }
+
+    const setCards = cardLists.map((_, listIndex) => {
+        return (newCards) => {
+            cardLists[listIndex].cards = newCards
+            setState({ ...state, cardLists })
+        }
+    })
+
+    const setCardLists = (newCardLists) => {
+        setState({ ...state, cardLists: newCardLists })
     }
 
     const addCard = (newCard) => {
-        cards.push(newCard)
-        setState({
-            ...state,
-            cards,
-            shownCreateCardModal: false        
-        })
+        cardLists[targetListIndex].cards.push(newCard)
+        setState({ ...state, cardLists, shownCreateCardModal: false })
     }
 
-    const removeCard = (index) => {
-        cards.splice(index, 1)
-        setState({
-            ...state,
-            cards
-        })
+    const removeCard = (listIndex, cardIndex) => {
+        cardLists[listIndex].cards.splice(cardIndex, 1)
+        setState({ ...state, cardLists })
     }
 
     return (
         <>
-            { shownCreateCardModal ?
+            {shownCreateCardListModal ? <CreateModal
+                onAdd={(newCardList) => addCardList(newCardList)}
+                onClose={closeCreateCardListModal}
+            /> : <></>}
+
+            {shownCreateCardModal ?
                 <CreateModal
                     onAdd={addCard}
                     onClose={closeCreateCardModal}
@@ -80,12 +139,49 @@ function App() {
             }
             <Navigation />
             <SCardLists>
-                <CardList>
-                    { cards.map((card, i) => <Card title={ card.title } key={ i } removeCard = { () => removeCard(i) } />)}
-                    <SButton onClick={ (e) => {
-                        openCreateCardModal()
-                    }}>追加</SButton>
-                </CardList>
+                <ReactSortable
+                    list={cardLists}
+                    setList={setCardLists} // あとで定義する
+                    group='cardList'
+                    animation={150}
+                    style={{ display: 'flex' }}
+                >
+
+                    {
+                        cardLists.map(({ title, cards }, listIndex) => {
+                            const cardComponents = cards.map((card, cardIndex) => <Card title={card.title} removeCard={() => {
+                                removeCard(listIndex, cardIndex)
+                            }} key={cardIndex} />)
+                            return (
+                                <CardList key={listIndex}>
+                                    <SListTitleWrapper>
+                                        <SListTitle>{title}</SListTitle>
+                                        <SRemoveCardListButton onClick={()=>removeCardList(listIndex)}>
+                                            削除
+                                        </SRemoveCardListButton>
+                                    </SListTitleWrapper>
+                                    <ReactSortable
+                                        list={cards}
+                                        setList={(newCards) => setCards[listIndex](newCards)}
+                                        group='card'
+                                        animation={150}
+                                        style={{ flexGrow: 1 }}
+                                    >
+                                        {cardComponents}
+                                    </ReactSortable>
+
+                                    <SButton onClick={(e) => {
+                                        openCreateCardModal(listIndex)
+                                    }}>追加</SButton>
+                                </CardList>
+                            )
+                        })
+                    }
+                </ReactSortable>
+
+                <SAddListButton onClick={(e) => {
+                    openCreateCardListModal()
+                }}>リストを追加</SAddListButton>
             </SCardLists>
         </>
     )
